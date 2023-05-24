b4a30a83ea4dc969c578624d6bea853ab21410b0
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-05-24 03:15:11 +0300

    appwrite utils :zap:

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,10 @@ export const Server = {
     endpoint : import.meta.env.VITE_API_APPWRITE,
     project : import.meta.env.VITE_PROJECT
 }
+
+import { Client,Account } from 'appwrite'
+
+const client = new Client()
+client.setEndpoint(Server.endpoint).setProject(Server.project)
+
+export const account = new Account(client);
